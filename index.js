22cdb44b824c583b4a70369c8613cb3390b324bb
Luca Sbardella <luca@quantmind.com>
2016-09-16 07:54:34 +0100

    added version

--- index.js
+++ index.js
@@ -4,3 +4,4 @@ export {default as assign} from './src/assign';
 export {default as self} from './src/self';
 export {default as pop} from './src/pop';
 export {isObject, isString, isFunction, isArray, isDate, isNumber, isPromise} from './src/inspect';
+export {version as letVersion} from './package.json';
